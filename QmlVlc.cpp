07e167bc46609eff72eb5af66b431a4ae9cc4f29
Sergey Radionov <RSATom@gmail.com>
2014-04-20 12:11:50 +0700

    register all used QmlVlc types

--- QmlVlc.cpp
+++ QmlVlc.cpp
@@ -13,19 +13,57 @@ const char* qmlVlcUri = "QmlVlc";
 const int QmlVlcVersionMajor = 0;
 const int QmlVlcVersionMinor = 1;
 
-void RegisterQmlVlcVideoSurface()
+void RegisterQmlVlcPlayer()
 {
+    qmlRegisterUncreatableType<QmlVlcAudio>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcAudio",
+        QStringLiteral("VlcAudio cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcInput>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcInput",
+        QStringLiteral("VlcInput cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcPlaylistItems>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcPlaylistItems",
+        QStringLiteral("VlcPlaylistItems cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcPlaylist>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcPlaylist",
+        QStringLiteral("VlcPlaylist cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcSubtitle>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcSubtitle",
+        QStringLiteral("VlcSubtitle cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcMarquee>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcMarquee",
+        QStringLiteral("VlcMarquee cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcLogo>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcLogo",
+        QStringLiteral("VlcLogo cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcDeinterlace>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcDeinterlace",
+        QStringLiteral("VlcDeinterlace cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcVideo>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcVideo",
+        QStringLiteral("VlcVideo cannot be instantiated directly") );
+    qmlRegisterUncreatableType<QmlVlcMediaDesc>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcMediaDesc",
+        QStringLiteral("VlcMediaDesc cannot be instantiated directly") );
+
+    qmlRegisterUncreatableType<QmlVlcSurfacePlayerProxy>(
+        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
+        "VlcSurfacePlayerProxy",
+        QStringLiteral("VlcSurfacePlayerProxy cannot be instantiated directly") );
+
     qmlRegisterType<QmlVlcVideoSurface>(
         qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
         "VlcVideoSurface" );
-}
-
-void RegisterQmlVlcPlayer()
-{
-    qmlRegisterUncreatableType<QmlVlcSurfacePlayerProxy>(
-        qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
-        "QmlVlcSurfacePlayerProxy",
-        QStringLiteral("QmlVlcSurfacePlayerProxy cannot be instantiated directly") );
     qmlRegisterType<QmlVlcPlayer>(
         qmlVlcUri, QmlVlcVersionMajor, QmlVlcVersionMinor,
         "VlcPlayer" );
@@ -42,7 +80,6 @@ void RegisterQmlVlcMmPlayer()
 
 void RegisterQmlVlc()
 {
-    RegisterQmlVlcVideoSurface();
     RegisterQmlVlcPlayer();
 
 #ifdef QMLVLC_QTMULTIMEDIA_ENABLE
